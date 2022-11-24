62a4e79b11f8de3a39ce6ff89024403834e2782c
Inderpreet <inderpreet.react.project@gmail.com>
2022-11-24 10:48:17 +0530

    update contact page

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -11,7 +11,7 @@ export default function ContactMe() {
 			<div className="flex flex-col gap-6 md:flex-row md:gap-12">
 				<form className="relative flex h-full w-full flex-col gap-14 pt-10 md:w-1/2 md:gap-10 md:pt-24">
 					<div className="flex h-12 flex-col gap-4 md:h-14 md:flex-row">
-						<span className="w-full self-center text-xl font-semibold md:w-1/3 md:self-start">
+						<span className="w-full self-center text-xl font-semibold text-neutral-300 md:w-1/3 md:self-start">
 							Name:
 						</span>
 						<input
@@ -22,7 +22,7 @@ export default function ContactMe() {
 					</div>
 
 					<div className="flex h-12 flex-col gap-4 md:h-14 md:flex-row">
-						<span className="w-full self-center text-xl font-semibold md:w-1/3 md:self-start">
+						<span className="w-full self-center text-xl font-semibold text-neutral-300 md:w-1/3 md:self-start">
 							E-mail:
 						</span>
 						<input
@@ -33,7 +33,7 @@ export default function ContactMe() {
 					</div>
 
 					<div className="flex h-32 flex-col gap-4 md:h-36 md:flex-row">
-						<span className="w-full self-center text-xl font-semibold md:w-1/3 md:self-start">
+						<span className="w-full self-center text-xl font-semibold text-neutral-300 md:w-1/3 md:self-start">
 							Message:
 						</span>
 						<textarea
@@ -49,7 +49,7 @@ export default function ContactMe() {
 					</div>
 				</form>
 				<div>
-					<h2 className="text-2xl font-semibold">Socials</h2>
+					<h2 className="text-2xl font-semibold underline">Socials</h2>
 					<div className="relative mt-4 flex flex-col gap-2 md:mt-16 md:gap-8">
 						<a
 							href="https://github.com/Inderpreet-React"
