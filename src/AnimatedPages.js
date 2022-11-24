75038f5041999b1b9a45c2de804ed87be549a507
Inderpreet <inderpreet.react.project@gmail.com>
2022-11-24 11:18:09 +0530

    animatedpresence added

--- src/AnimatedPages.js
+++ src/AnimatedPages.js
@@ -3,13 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import AboutMe from "./components/AboutMe";
 import ContactMe from "./components/ContactMe";
 import Projects from "./components/Projects";
+import { useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
 export default function AnimatedPages() {
+	const location = useLocation();
+
 	return (
-		<Routes>
-			<Route path="/" element={<AboutMe />} />
-			<Route path="/projects" element={<Projects />} />
-			<Route path="contactme" element={<ContactMe />} />
-		</Routes>
+		<AnimatePresence>
+			<Routes location={location} key={location.pathname}>
+				<Route path="/" element={<AboutMe />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="contactme" element={<ContactMe />} />
+			</Routes>
+		</AnimatePresence>
 	);
 }
