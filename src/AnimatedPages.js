04f54d8da23110dbca8dc8521f875d97e371432c
Inderpreet <inderpreet.react.project@gmail.com>
2022-11-24 11:08:48 +0530

    added react router dom

---
+++ src/AnimatedPages.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import AboutMe from "./components/AboutMe";
+import ContactMe from "./components/ContactMe";
+import Projects from "./components/Projects";
+
+export default function AnimatedPages() {
+	return (
+		<Routes>
+			<Route path="/" element={<AboutMe />} />
+			<Route path="/projects" element={<Projects />} />
+			<Route path="contactme" element={<ContactMe />} />
+		</Routes>
+	);
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import Navbar from "./components/Navbar";
-import AboutMe from "./components/AboutMe";
-import PurpleBlob from "./components/PurpleBlob";
-import YellowBlob from "./components/YellowBlob";
-import ContactMe from "./components/ContactMe";
-import Projects from "./components/Projects";
+import AnimatedPages from "./AnimatedPages";
 
 function App() {
 	return (
 		<div className="">
 			<div className="flex h-screen flex-col">
 				<Navbar />
-				<div className="relative mt-24 flex flex-grow flex-col overflow-hidden bg-neutral-900 text-white">
-					<AboutMe />
-					<PurpleBlob />
-					<YellowBlob />
-				</div>
+				<AnimatedPages />
 			</div>
-			<Projects />
-			<ContactMe />
 		</div>
 	);
 }
