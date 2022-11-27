5de47619360822a439ac8df19044a6b5d8c6e82f
Inderpreet <inderpreet.react.project@gmail.com>
2022-11-27 16:26:34 +0530

    navbar mobile design update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 export default function Navbar() {
+	const location = useLocation();
+
 	return (
 		<div className="fixed top-0 left-0 z-10 flex h-24 w-full items-center justify-between border-b border-gray-500 bg-black p-4 text-white md:p-8">
 			<Link
@@ -10,14 +12,14 @@ export default function Navbar() {
 			>
 				Inderpreet
 			</Link>
-			<ul className="hidden gap-20 md:visible md:flex">
+			<ul className="flex gap-2 md:flex md:gap-20">
 				<li className="flex cursor-pointer rounded  bg-neutral-800 transition-colors hover:bg-rose-500">
 					<NavLink
 						to=""
 						className={({ isActive }) =>
 							isActive
-								? "flex h-full w-24 items-center justify-center rounded bg-rose-500 p-2 text-sm font-semibold text-white opacity-100"
-								: "flex h-full w-24 items-center justify-center p-2 text-sm font-semibold text-white opacity-80"
+								? "flex h-full items-center justify-center rounded bg-rose-500 p-2 text-xs font-semibold text-white opacity-100 md:w-24 md:text-sm"
+								: "flex h-full items-center justify-center p-2 text-xs font-semibold text-white opacity-80 md:w-24 md:text-sm"
 						}
 					>
 						About me
@@ -28,8 +30,8 @@ export default function Navbar() {
 						to="skills"
 						className={({ isActive }) =>
 							isActive
-								? "flex h-full w-24 items-center justify-center rounded bg-amber-500 p-2 text-sm font-semibold text-white opacity-100"
-								: "flex h-full w-24 items-center justify-center p-2 text-sm font-semibold text-white opacity-80"
+								? "flex h-full items-center justify-center rounded bg-amber-500 p-2 text-xs font-semibold text-white opacity-100 md:w-24 md:text-sm"
+								: "flex h-full items-center justify-center p-2 text-xs font-semibold text-white opacity-80 md:w-24 md:text-sm"
 						}
 					>
 						Skills
@@ -40,15 +42,19 @@ export default function Navbar() {
 						to="projects"
 						className={({ isActive }) =>
 							isActive
-								? "flex h-full w-24 items-center justify-center rounded bg-emerald-500 p-2 text-sm font-semibold text-white opacity-100"
-								: "flex h-full w-24 items-center justify-center p-2 text-sm font-semibold text-white opacity-80"
+								? "flex h-full items-center justify-center rounded bg-emerald-500 p-2 text-xs font-semibold text-white opacity-100 md:w-24 md:text-sm"
+								: "flex h-full items-center justify-center p-2 text-xs font-semibold text-white opacity-80 md:w-24 md:text-sm"
 						}
 					>
 						Projects
 					</NavLink>
 				</li>
 			</ul>
-			<div className="group relative">
+			<div
+				className={`${
+					location.pathname === "/contactme" ? "invisible md:visible" : ""
+				} group fixed bottom-16 right-8 z-20 md:visible md:relative md:bottom-auto md:right-auto`}
+			>
 				<span className="group:duration-200 absolute -inset-2 animate-tilt rounded-lg bg-gradient-to-r from-pink-600 to-purple-600 blur group-hover:-inset-2.5"></span>
 				<NavLink
 					to="contactme"
