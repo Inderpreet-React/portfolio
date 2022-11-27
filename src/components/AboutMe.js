ba8467b1f6e572342d7bf7b743998265493ecf84
Inderpreet <inderpreet.react.project@gmail.com>
2022-11-28 01:37:40 +0530

    about me design update

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,14 +12,26 @@ export default function AboutMe() {
 		>
 			<div className="relative flex h-screen flex-col bg-neutral-900 pt-24 text-white">
 				<div className="relative flex h-full w-full flex-col items-center justify-center gap-8 p-4 md:p-8">
-					<h1 className="w-full text-left text-6xl font-extrabold md:w-1/2">
+					<h1 className="relative w-full text-left text-6xl font-extrabold md:w-1/2 md:text-8xl">
+						<span className="absolute -top-6 -left-2 text-lg font-normal text-neutral-700 md:-left-8">
+							{" </h1>"}
+						</span>
 						Hey,
+						<span className="absolute -bottom-6 -left-2 text-lg font-normal text-neutral-700 md:-left-8">
+							{" </h1>"}
+						</span>
 					</h1>
-					<h1 className="w-full whitespace-normal text-3xl font-extrabold md:w-1/2 md:text-6xl">
-						My Name is Inderpreet
+					<h1 className=" w-full bg-gradient-to-r from-pink-500 to-violet-500 text-left text-5xl font-extrabold md:w-1/2 md:text-8xl">
+						I'm Inderpreet.
 					</h1>
-					<h1 className="w-full whitespace-normal text-2xl font-extrabold md:w-1/2 md:text-4xl">
-						A frontend developer
+					<h1 className="relative flex w-full flex-col whitespace-normal text-2xl font-semibold text-neutral-400 md:w-1/2 md:text-4xl">
+						<span className="absolute -top-6 -left-2 text-lg font-normal text-neutral-700 md:-left-8">
+							{"<p> "}
+						</span>
+						A Web developer
+						<span className="absolute -bottom-6 -left-2 text-lg font-normal text-neutral-700 md:-left-8">
+							{" </p>"}
+						</span>
 					</h1>
 				</div>
 				<PurpleBlob />
