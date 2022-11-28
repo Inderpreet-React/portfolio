16212f0afac0123e855d473bb4e4777c22bdd535
Inderpreet <inderpreet.react.project@gmail.com>
2022-11-29 00:23:01 +0530

    update border-color

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -7,7 +7,7 @@ export default function ProjectItem({ img, name, color }) {
 			<span className="relative z-10 min-w-full max-w-min rounded bg-violet-700 p-2 text-2xl font-bold tracking-normal text-gray-100 transition-all">
 				{name || "Name"}
 			</span>
-			<div className="relative z-20 overflow-hidden border-8 border-rose-500 transition-all group-hover:shadow-lg group-hover:shadow-rose-800">
+			<div className="relative z-20 overflow-hidden border-8 border-neutral-600 transition-all group-hover:shadow-lg group-hover:shadow-violet-600">
 				<img
 					className={`aspect-video w-full  transition-all group-hover:scale-105 md:h-full`}
 					src={img}
