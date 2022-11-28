95966c2ed922f021f74761f796380681d5bf7a6d
Inderpreet <inderpreet.react.project@gmail.com>
2022-11-28 23:13:20 +0530

    minor design tweaks

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -66,45 +66,45 @@ export default function Skills() {
 						</span>
 					</p>
 				</div>
-				<div className="mb-4 flex w-full flex-col gap-4 pb-2 md:mb-0 md:w-1/2 md:pb-0">
+				<div className="mb-4 flex w-full flex-col gap-4 pb-4 md:mb-0 md:w-1/2 md:pb-0">
 					<h3 className="relative max-w-min text-3xl font-bold text-white after:absolute after:-bottom-1 after:left-0 after:h-1 after:w-full after:rounded-md after:bg-gradient-to-l after:from-pink-500 after:to-violet-500 md:pt-10">
 						Technologies
 					</h3>
-					<div className="relative md:ml-8">
-						<span className="absolute -top-4 -left-2 text-lg font-normal italic text-neutral-700 md:-left-8">
+					<div className="relative text-xl font-bold tracking-widest md:ml-12 md:font-extrabold">
+						<span className="absolute -top-4 -left-0 text-lg font-normal italic text-neutral-700 md:-top-3 md:-left-12">
 							{"<ul>"}
 						</span>
-						<div className="relative mt-4 flex items-center gap-4 border-2 border-yellow-300 p-4 text-lg font-semibold shadow-sm shadow-yellow-300 transition-all after:absolute after:right-0 after:top-0 after:h-full after:w-4 after:bg-yellow-300 hover:shadow-lg hover:shadow-yellow-300 md:w-96">
+						<div className="relative mt-4 flex items-center gap-4 border-2 border-yellow-300 p-4 shadow-sm shadow-yellow-300 transition-all after:absolute after:right-0 after:top-0 after:h-full after:w-4 after:bg-yellow-300 hover:shadow-lg hover:shadow-yellow-300 md:mr-32">
 							<div className="w-20">
 								<JavascriptLogo hw={12} />
 							</div>
 							<span>JavaScript</span>
 						</div>
-						<div className="relative mt-4 flex items-center gap-4 border-2 border-cyan-300 p-4 text-lg font-semibold shadow-sm shadow-cyan-300 transition-all after:absolute after:right-0 after:top-0 after:h-full after:w-4 after:bg-cyan-300 hover:shadow-lg hover:shadow-cyan-300 md:w-96">
+						<div className="relative mt-4 flex items-center gap-4 border-2 border-cyan-300 p-4 shadow-sm shadow-cyan-300 transition-all after:absolute after:right-0 after:top-0 after:h-full after:w-4 after:bg-cyan-300 hover:shadow-lg hover:shadow-cyan-300 md:mr-32">
 							<div className="w-20">
 								<ReactIcon hw={12} />
 							</div>
 							<span>React</span>
 						</div>
-						<div className="relative  mt-4 flex items-center gap-4 border-2 border-lime-400 p-4 text-lg font-semibold shadow-sm shadow-lime-400 transition-all after:absolute after:right-0 after:top-0 after:h-full after:w-4 after:bg-lime-400 hover:shadow-lg hover:shadow-lime-400  md:w-96">
+						<div className="relative  mt-4 flex items-center gap-4 border-2 border-lime-400 p-4 shadow-sm shadow-lime-400 transition-all after:absolute after:right-0 after:top-0 after:h-full after:w-4 after:bg-lime-400 hover:shadow-lg hover:shadow-lime-400  md:mr-32">
 							<div className="w-20">
 								<NodeIcon hw={12} />
 							</div>
 							<span>NodeJS</span>
 						</div>
-						<div className="relative mt-4 flex items-center gap-4 border-2 border-amber-400 p-4 text-lg font-semibold shadow-sm shadow-amber-400 transition-all after:absolute after:right-0 after:top-0 after:h-full after:w-4 after:bg-amber-400 hover:shadow-lg hover:shadow-amber-400  md:w-96">
+						<div className="relative mt-4 flex items-center gap-4 border-2 border-amber-400 p-4 shadow-sm shadow-amber-400 transition-all after:absolute after:right-0 after:top-0 after:h-full after:w-4 after:bg-amber-400 hover:shadow-lg hover:shadow-amber-400  md:mr-32">
 							<div className="w-20">
 								<FirebaseLogo hw={12} />
 							</div>
 							<span>Firebase</span>
 						</div>
-						<div className="relative mt-4 flex items-center gap-4 border-2 border-cyan-300 p-4 text-lg font-semibold shadow-sm shadow-cyan-300 transition-all after:absolute after:right-0 after:top-0 after:h-full after:w-4 after:bg-cyan-300 hover:shadow-lg hover:shadow-cyan-300  md:w-96">
+						<div className="relative mt-4 flex items-center gap-4 border-2 border-cyan-300 p-4 shadow-sm shadow-cyan-300 transition-all after:absolute after:right-0 after:top-0 after:h-full after:w-4 after:bg-cyan-300 hover:shadow-lg hover:shadow-cyan-300  md:mr-32">
 							<div className="w-20">
 								<img src={TailwindIcon} alt="Tailwind" className="h-10 w-10" />
 							</div>
 							<span>Tailwind</span>
 						</div>
-						<span className="absolute -bottom-6 -left-2 text-lg font-normal italic text-neutral-700 md:-left-8">
+						<span className="absolute -left-0 -bottom-8 text-lg font-normal italic text-neutral-700 md:-left-12">
 							{"</ul>"}
 						</span>
 					</div>
