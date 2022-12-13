815775235ba1cc2ec151d5fd49ffb26bd94943e6
Inderpreet <inderpreet.react.project@gmail.com>
2022-12-14 02:31:01 +0530

    update design

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,30 +1,51 @@
 import React from "react";
 import { motion } from "framer-motion";
+import GithubIcon from "../images/GithubIcon";
 
 const variants = {
 	entry: { y: "30%", scale: 1.01, opacity: 0 },
 	exit: { y: 0, scale: 1, opacity: 1 },
+	hover: { y: "-5%" },
 };
 
-export default function ProjectItem({ img, name, color }) {
+export default function ProjectItem({ img, name, link }) {
 	return (
 		<motion.div
 			initial="entry"
 			whileInView="exit"
+			whileHover="hover"
 			transition={{ duration: "0.3", ease: "linear" }}
 			variants={variants}
 			viewport={{ once: true }}
-			className="group h-full w-full rounded transition-all hover:-translate-y-4"
+			className="group relative h-full w-full rounded"
 		>
-			<span className="relative z-10 min-w-full max-w-min rounded bg-violet-700 p-2 text-2xl font-bold tracking-normal text-gray-100 transition-all">
-				{name || "Name"}
-			</span>
-			<div className="relative z-20 overflow-hidden border-8 border-neutral-600 transition-all group-hover:shadow-lg group-hover:shadow-violet-500">
+			<div className="relative z-20 overflow-hidden transition-all group-hover:shadow-lg group-hover:shadow-amber-200">
 				<img
-					className={`aspect-video w-full  transition-all group-hover:scale-105 md:h-full`}
+					className="aspect-video w-full rounded md:h-full"
 					src={img}
 					alt="project"
 				/>
+			</div>
+			<div className="absolute top-0 left-0 z-30 h-full w-full bg-[#00000080] opacity-0 transition-all group-hover:opacity-100">
+				<div>
+					<span
+						style={{ clipPath: "polygon(0 0, 100% 0, 100% 21%, 0% 100%)" }}
+						className="absolute left-0 top-0 h-1/2 w-full rounded bg-[#f59e0be6] pl-2 text-3xl font-bold tracking-normal text-gray-100"
+					>
+						{name || "Name"}
+					</span>
+					<div
+						style={{ clipPath: "polygon(100% 0, 0% 100%, 100% 100%)" }}
+						className="absolute bottom-0 right-0 h-1/3 w-full bg-[#f59e0be6]"
+					>
+						<a
+							href={link || "#"}
+							className="absolute right-2 bottom-2 h-10 w-10 fill-white"
+						>
+							<GithubIcon cls="h-10 w-10 hover:scale-110 transition-all" />
+						</a>
+					</div>
+				</div>
 			</div>
 		</motion.div>
 	);
