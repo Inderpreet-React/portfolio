c077d17ba04063b2e01ddd0abbfc430c77174179
Inderpreet <inderpreet.react.project@gmail.com>
2022-12-14 02:39:28 +0530

    design update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,23 +8,23 @@ export default function Navbar() {
 		<div className="fixed top-0 left-0 z-50 flex h-24 w-full items-center justify-between border-b border-gray-500 bg-black p-4 text-white md:p-8">
 			<a
 				href="#aboutme"
-				className="bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-2xl font-extrabold text-transparent"
+				className="relative text-2xl font-extrabold text-white after:absolute after:-bottom-1 after:left-0 after:h-1 after:w-full after:rounded-md after:bg-gradient-to-l after:from-pink-500 after:to-violet-500"
 			>
 				Inderpreet
 			</a>
 			<ul className="flex gap-2 md:flex md:gap-20">
-				<li className="flex cursor-pointer justify-center  rounded bg-neutral-800 transition-colors hover:bg-rose-500">
-					<a className="text-center md:w-24 md:p-1" href="#aboutme">
+				<li className="flex cursor-pointer justify-center  rounded bg-neutral-800 transition-colors hover:bg-amber-500">
+					<a className="p-1 text-center md:w-24 md:p-1" href="#aboutme">
 						About me
 					</a>
 				</li>
 				<li className="flex cursor-pointer justify-center rounded bg-neutral-800 transition-colors hover:bg-amber-500">
-					<a className="text-center md:w-24 md:p-1" href="#skills">
+					<a className="p-1 text-center md:w-24 md:p-1" href="#skills">
 						Skills
 					</a>
 				</li>
-				<li className="flex cursor-pointer justify-center rounded bg-neutral-800 transition-colors hover:bg-emerald-500">
-					<a className="text-center md:w-24 md:p-1" href="#projects">
+				<li className="flex cursor-pointer justify-center rounded bg-neutral-800 transition-colors hover:bg-amber-500">
+					<a className="p-1 text-center md:w-24 md:p-1" href="#projects">
 						Projects
 					</a>
 				</li>
