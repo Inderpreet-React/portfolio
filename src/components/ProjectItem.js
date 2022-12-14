16a873584778b5c3dbee9110810ba4a4cb51a689
Inderpreet <inderpreet.react.project@gmail.com>
2022-12-14 11:04:59 +0530

    minor design update

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -29,18 +29,18 @@ export default function ProjectItem({ img, name, link }) {
 			<div className="absolute top-0 left-0 z-30 h-full w-full bg-[#00000080] opacity-0 transition-all group-hover:opacity-100">
 				<div>
 					<span
-						style={{ clipPath: "polygon(0 0, 100% 0, 100% 21%, 0% 100%)" }}
-						className="absolute left-0 top-0 h-1/2 w-full rounded bg-[#f59e0be6] pl-2 text-3xl font-bold tracking-normal text-gray-100"
+						style={{ clipPath: "polygon(0 0, 100% 0, 100% 30%, 0% 100%)" }}
+						className="absolute left-0 top-0 h-1/2 w-full rounded bg-[#f59e0be6] pl-2 pt-2 text-3xl font-bold tracking-normal text-gray-100 underline decoration-4 underline-offset-8"
 					>
 						{name || "Name"}
 					</span>
 					<div
-						style={{ clipPath: "polygon(100% 0, 0% 100%, 100% 100%)" }}
+						style={{ clipPath: "polygon(0 75%, 100% 0, 100% 100%, 0% 100%)" }}
 						className="absolute bottom-0 right-0 h-1/3 w-full bg-[#f59e0be6]"
 					>
 						<a
 							href={link || "#"}
-							className="absolute right-2 bottom-2 h-10 w-10 fill-white"
+							className="absolute right-4 bottom-4 h-10 w-10 fill-white"
 						>
 							<GithubIcon cls="h-10 w-10 hover:scale-110 transition-all" />
 						</a>
